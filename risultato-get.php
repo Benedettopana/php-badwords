aecd3b1a67267334d79bd6cb17268576ad0f4afb
Benedettopana <benedetto.pana@gmail.com>
2024-04-12 16:46:54 +0200

    [add]Sostituzione badword

--- risultato-get.php
+++ risultato-get.php
@@ -2,8 +2,10 @@
 $text = $_GET['text'];
 $badWord = $_GET['badWord'];
 
+$text_length = strlen($text);
 // var_dump($text);
 // var_dump($badWord);
+$text_censured = str_replace($badWord, '***',$text);
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +16,10 @@ $badWord = $_GET['badWord'];
   <title>Risultato get</title>
 </head>
 <body>
-  <h1>Testo:</h1>
-  <h3> <?php echo $text ?></h3>
+  <h1 class="my-5">Risultato</h1>
+  <h3 class="my-2"> <?php echo  $text ?></h3>
+  <h3 >La lunghezza del testo inserito è: <?php echo $text_length ?></h3>
+  <h3 class="my-5">Risultato della censura:</h3>
+  <p><?php echo $text_censured  ?></p>
 </body>
 </html>
